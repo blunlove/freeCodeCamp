07b122684e7f5555096577d7025bb5cad6477230
alun <314884680@qq.com>
2019-06-05 17:29:07 +0800

    新增两个项目

---
+++ simonGame/index.js
@@ -0,0 +1,21 @@
+function main() {
+  new Vue({
+    el: '#app',
+    data() {
+      return {
+      }
+    },
+    methods: {
+    },
+    template: `
+      <div class="app">
+        <div class="app-header">
+          Simon Game
+        </div>
+        <div class="app-content">
+        </div>
+      </div>
+    `,
+  });
+}
+main();
